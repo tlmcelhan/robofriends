d7f9a514411dcd4814459c4058899f3ec1b2259a
tlmcelhan <tlmcelhan@gmail.com>
2021-11-04 16:40:10 -0600

    fix bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox'
 import './App.css';
 import Scroll from '../components/scroll'
-import ErrorBoundry from '../components/ErrorBoundry'
+// import ErrorBoundry from '../components/ErrorBoundry'
 
 
 class App extends Component {
