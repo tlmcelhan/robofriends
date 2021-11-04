cbce85154eff194e26a13a9a819fe1f0215ed492
tlmcelhan <tlmcelhan@gmail.com>
2021-11-04 16:22:38 -0600

    fix error

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,9 +36,7 @@ class App extends Component {
                 <h1 className= 'f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <ErrorBoundry>
                     <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
